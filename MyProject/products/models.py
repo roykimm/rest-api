dbb78eac4dfd783a2e97bb5522f8a4677e9c4466
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-08-04 23:42:16 +0900

    tt

---
+++ MyProject/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    image = models.ImageField(upload_to='uploads/images', null=False, blank=False)
+    name = models.CharField(max_length=150, null=False, blank=False)
+    price = models.DecimalField(max_digits=14, decimal_places=2, null=False, blank=False)
+    description = models.TextField()
+    category = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
