aa3de2c89d314a40a5e92c428044e19b6256b9cd
Myunghoon kim <roykimmyunghoon@gmail.com>
2021-08-05 08:50:11 +0900

    12

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -4,7 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from products.views import ProductView
+from rest_framework import routers
+
+route = routers.DefaultRouter()
+route.register("", ProductView, basename='productview')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api_basic.urls')),
+    path('api/', include(route.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- MyProject/products/views.py
+++ MyProject/products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class ProductView(viewsets.ModelViewSet):
+    #queryset = Product.objects.all().order_by('-id')
+    #queryset = Product.objects.all().filter
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
